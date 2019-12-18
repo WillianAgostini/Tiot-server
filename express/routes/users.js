2f7a1f6a1e8a3dc7f6dd8607239d980ecaac2406
willian <willian>
2019-12-18 19:12:58 -0300

    adicionado rota para add deviceName

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const User = require("../models/user");
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
 });
+
+router.post(
+  "/deviceName",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true
+  }),
+  (req, res, next) => {
+    req.session.save(err => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  }
+);
 
 module.exports = router;
