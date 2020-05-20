4ca42650193f2b7b8a420a03e20dddc38aa9a87b
Willian <willian.agostini@gmail.com>
2020-05-20 16:09:15 -0300

    identação

--- express/app.js
+++ express/app.js
@@ -1,36 +1,35 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var cors = require("cors");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
 // mongoose
 mongoose
-  .connect("mongodb://localhost/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(
-    () => {
-      console.log("mongoose ready to use!");
-    },
-    err => {
-      console.error(err);
-    }
-  );
+    .connect('mongodb://localhost/test', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    .then(
+        () => {
+          console.log('mongoose ready to use!');
+        },
+        err => {
+          console.error(err);
+        });
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 module.exports = app;
 
-require("./config/routes")(app);
+require('./config/routes')(app);
