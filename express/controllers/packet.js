7e80a1bb9003b6b4f3f339a322a489525debecc6
Willian <willian.agostini@gmail.com>
2020-07-17 12:46:03 -0300

    otimização

--- express/controllers/packet.js
+++ express/controllers/packet.js
@@ -33,43 +33,72 @@ exports.create = function(req, res, next) {
   });
 };
 
-exports.list = function(req, res, next) {
+exports.list = async (req, res, next) => {
   let name = req.params.name;
-
   let min_date = new Date(req.params.min_date);
-  Packet.find({deviceName: name, createDate: {$gte: min_date}})
-      .sort('createDate')
-      .exec(function(err, packets) {
-        if (err) res.sendStatus(404);
+  const diffTime = Math.abs(new Date() - min_date);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const now = new Date();
+  let response = [];
 
-        const diffTime = Math.abs(new Date() - min_date);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        const now = new Date();
-        let response = [];
+  try {
+    for (let index = 0; index < diffDays; index++) {
+      let indexDateStart =
+          new Date(now.getFullYear(), now.getMonth(), now.getDate() - index);
+      let indexDateEnd = new Date(
+          now.getFullYear(), now.getMonth(), now.getDate() - index + 1);
+      let packet =
+          await Packet
+              .findOne({createDate: {$gte: indexDateStart, $lt: indexDateEnd}})
+              .exec()
+      if (packet) {
+        response.push(packet);
+      }
+      else {
+        response.push(new Packet({
+          payload: undefined,
+          deviceName: undefined,
+          status: undefined,
+          createDate: indexDateStart
+        }))
+      }
+    }
 
-        for (let index = 0; index < diffDays; index++) {
-          let indexDate = new Date(
-              now.getFullYear(), now.getMonth(), now.getDate() - index);
+    return res.status(200).json(response);
 
-          let packet = packets.find(
-              x => x.createDate.getFullYear() == indexDate.getFullYear() &&
-                  x.createDate.getMonth() == indexDate.getMonth() &&
-                  x.createDate.getDay() == indexDate.getDay());
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+  // Packet.find({deviceName: name, createDate: {$gte: min_date}})
+  //     .sort('createDate')
+  //     .exec(function(err, packets) {
+  //       if (err) res.sendStatus(404);
 
-          if (packet != undefined) {
-            response.push(packet);
-          } else {
-            response.push(new Packet({
-              payload: undefined,
-              deviceName: undefined,
-              status: undefined,
-              createDate: indexDate
-            }))
-          }
-        }
 
-        return res.status(200).json(response);
-      });
+
+  //       for (let index = 0; index < diffDays; index++) {
+  //         let indexDate = new Date(
+  //             now.getFullYear(), now.getMonth(), now.getDate() - index);
+
+  //         let packet = packets.find(
+  //             x => x.createDate.getFullYear() == indexDate.getFullYear() &&
+  //                 x.createDate.getMonth() == indexDate.getMonth() &&
+  //                 x.createDate.getDay() == indexDate.getDay());
+
+  //         if (packet != undefined) {
+  //           response.push(packet);
+  //         } else {
+  //           response.push(new Packet({
+  //             payload: undefined,
+  //             deviceName: undefined,
+  //             status: undefined,
+  //             createDate: indexDate
+  //           }))
+  //         }
+  //       }
+
+  //       return res.status(200).json(response);
+  //     });
 };
 
 exports.getPackets =
